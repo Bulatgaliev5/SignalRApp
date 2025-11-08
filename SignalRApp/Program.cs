290abe91755bf3d1ff753014adccbb3bda306f3a
Admin <Admin@DESKTOP-V6V9F0T>
2025-11-08 14:52:22 +0500

    Добавьте файлы проекта.

---
+++ SignalRApp/Program.cs
@@ -0,0 +1,14 @@
+namespace SignalRApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddSignalR();
+            var app = builder.Build();
+            app.MapHub<ChatHub>("/chat");
+            app.Run();
+        }
+    }
+}
